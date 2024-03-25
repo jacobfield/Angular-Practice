852830f12be438bee23f47b6dceec54c0959256b
Jake Field <jacobprogrammes@gmail.com>
2024-03-25 11:23:36 +0000

    housing location comp added

---
+++ my-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>housing-location twerks!</p>
+    <p>hello there</p>
+  `,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {}
